ac8106c4f15ba55d8e87570ea21144f8b68d3be9
venkhatesh <venkhatesharunachalam@gmail.com>
2020-06-02 23:03:10 +0530

    KotlinChallenge: feat: add Coroutine Network Calls in ViewModel.

--- app/src/main/java/com/example/kotlinchallenge/ui/contest/ContestViewModel.kt
+++ app/src/main/java/com/example/kotlinchallenge/ui/contest/ContestViewModel.kt
@@ -1,10 +1,27 @@
 package com.example.kotlinchallenge.ui.contest
 
 import androidx.lifecycle.ViewModel
+import com.example.kotlinchallenge.data.repositories.ContestRepository
 import com.example.kotlinchallenge.ui.NetworkListener
+import com.example.kotlinchallenge.util.Coroutines
 
 class ContestViewModel : ViewModel() {
     var networkListener:NetworkListener ?= null
 
+    fun callOngoinfApi(){
+        Coroutines.main {
+            networkListener?.onStarted()
+            val onGoingResponse = ContestRepository().fetchOngoingContest()
+            networkListener?.onSuccess()
+        }
+    }
+
+    fun callUpcomingApi(){
+        Coroutines.main {
+            networkListener?.onStarted()
+            val onGoingResponse = ContestRepository().fetchUpcomingContest()
+            networkListener?.onSuccess()
+        }
+    }
 
 }
