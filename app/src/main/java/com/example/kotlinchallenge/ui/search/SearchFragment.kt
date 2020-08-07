a3107e84329a2fb6ff16aa2b0432038716592d9c
venkhatesh <venkhatesharunachalam@gmail.com>
2020-08-07 21:55:11 +0530

    CodeChefApp feat: add toggle switch library

--- app/src/main/java/com/example/kotlinchallenge/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/kotlinchallenge/ui/search/SearchFragment.kt
@@ -1,19 +1,26 @@
 package com.example.kotlinchallenge.ui.search
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.kotlinchallenge.R
+import com.google.android.material.slider.Slider
+import com.llollox.androidtoggleswitch.widgets.ToggleSwitch
+import com.llollox.androidtoggleswitch.widgets.ToggleSwitchButton
 import kotlinx.android.synthetic.main.fragment_search.*
+
 
 /**
  * Created by Venkhatesh on 06-08-2020.
  */
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(), ToggleSwitch.OnChangeListener {
 
+    val TAG = "SearchFragment"
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,10 +30,39 @@ class SearchFragment : Fragment() {
 
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        lottie_test.setAnimation("price_tag.json")
-        lottie_test.playAnimation()
-        lottie_test.loop(true)
+        lottie_animation.setAnimation("profile.json")
+        lottie_animation.playAnimation()
+        lottie_animation.loop(false)
+        val test = activity?.findViewById<ToggleSwitchButton>(R.id.toggle_switch_btn)
+        Log.d(TAG, "onViewCreated: Test Test")
+        Log.d(TAG, "onViewCreated: ")
+        toggle_switch_btn.setOnClickListener(View.OnClickListener {
+            Log.d(TAG, "onViewCreated: ")
+        })
+        toggle_switch_btn.setOnTouchListener(View.OnTouchListener { v, event ->
+           Log.d(TAG, "onViewCreated: insdieeee")
+           true
+           
+       })
+        test?.setOnClickListener {
+            Log.d(TAG, "onViewCreated: TEST TEST")
+        }
+        toggle_switch_btn.setOnClickListener {
+            Log.d(TAG, "onViewCreated: INSIDE")
+        }
+//        toggle_switch_btn.setOnToggleSwitchChangeListener { position, isChecked ->
+//            // Write your code ...
+//            Log.d(TAG, "onViewCreated: POsition CHeck $position")
+//        }
+
+    }
+
+    override fun onToggleSwitchChanged(position: Int) {
+        Log.d(TAG, "onToggleSwitchChanged: AIYO")
     }
 }
+
+
