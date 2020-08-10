7446ef8b4624c9f4c03c45175c691ea71e32acfd
venkhatesh <venkhatesharunachalam@gmail.com>
2020-08-11 02:20:37 +0530

    CodeChefApp feat: Hilt Dagger Init

---
+++ app/src/main/java/com/example/kotlinchallenge/CodeChefApplication.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinchallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Venkhatesh on 11-08-2020.
+ */
+
+@HiltAndroidApp
+class CodeChefApplication : Application() {
+}
