19302994adb45b2fc23b501f2b6aa1d9c14c7b5e
Venkhatesh arunachalam <venkhatesharunachalam@gmail.com>
2020-09-25 22:21:58 +0530

    CodeChefApp add: add Updated ScreenShots

--- app/src/main/java/com/example/kotlinchallenge/data/network/CodeChefApi.kt
+++ app/src/main/java/com/example/kotlinchallenge/data/network/CodeChefApi.kt
@@ -2,6 +2,7 @@ package com.example.kotlinchallenge.data.network
 
 import com.example.kotlinchallenge.data.network.responses.DataResponse
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
@@ -24,10 +25,19 @@ interface CodeChefApi {
     suspend fun past() : Response<DataResponse>
 
     companion object{
+
+        val okHttpClient = OkHttpClient().newBuilder()
+            .connectTimeout(60, java.util.concurrent.TimeUnit.SECONDS)
+            .writeTimeout(60,java.util.concurrent.TimeUnit.SECONDS)
+            .readTimeout(60,java.util.concurrent.TimeUnit.SECONDS)
+            .build()
+
+
         operator fun invoke() : CodeChefApi {
             return Retrofit.Builder()
                 .baseUrl("http://intense-citadel-99793.herokuapp.com/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build()
                 .create(CodeChefApi::class.java)
         }
